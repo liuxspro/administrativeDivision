3c027fdf790d0d565b90f451113465ee7cffad3e
liuxspro <liuxspro@gmail.com>
2024-06-12 10:20:14 +0800

    add release script and ci

--- script/convert.py
+++ script/convert.py
@@ -1,3 +1,5 @@
+import shutil
+import zipfile
 from pathlib import Path
 
 import geopandas as gpd
@@ -9,8 +11,6 @@ dist_path = cwd.joinpath("dist")
 
 
 def convert_to_shp(filepath: Path):
-    if not dist_path.exists():
-        dist_path.mkdir()
     subfolder = dist_path.joinpath(f"{filepath.stem}/")
     if not subfolder.exists():
         subfolder.mkdir()
@@ -32,29 +32,69 @@ def convert_to_shp(filepath: Path):
         driver="ESRI Shapefile",
         encoding="utf-8",
     )
+    # 打包为zip
+    # with zipfile.ZipFile(
+    #     dist_path.joinpath(f"{filepath.stem}.zip"), "w", zipfile.ZIP_DEFLATED
+    # ) as zipf:
+    #     for file in subfolder.glob("**/*"):
+    #         zipf.write(file, file.relative_to(subfolder.parent))
 
 
 def convert_to_gpkg(filepath: Path):
     gdf = gpd.read_file(filepath)
+    gpkg_file_path = dist_path.joinpath("天地图-行政区划.gpkg")
     multipolygons = gdf[gdf.geometry.type == "MultiPolygon"]
     multilinestring = gdf[gdf.geometry.type == "MultiLineString"]
     multipolygons.to_file(
-        dist_path.joinpath("天地图-行政区划.gpkg"),
+        gpkg_file_path,
         layer=f"{filepath.stem}",
         driver="GPKG",
     )
     multilinestring.to_file(
-        dist_path.joinpath("天地图-行政区划.gpkg"),
+        gpkg_file_path,
         layer=f"{filepath.stem}.境界线",
         driver="GPKG",
     )
 
 
-for i in geojson_path.glob("*.geojson"):
-    print(f"开始转换 {i.name}")
-    convert_to_shp(i)
-    print(f"{i.stem} shp 转换完成")
-    convert_to_gpkg(i)
-    print(f"{i.stem} gpkg 转换完成")
+def main():
+    # 删除 dist 文件夹
+    if dist_path.exists():
+        shutil.rmtree(dist_path)
+    # 创建 dist 文件夹
+    if not dist_path.exists():
+        dist_path.mkdir()
 
-# print(geojson_path.glob("*.geojson"))
+    for i in geojson_path.glob("*.geojson"):
+        print(f"开始转换 {i.name}")
+        convert_to_shp(i)
+        print(f"{i.stem} shp 转换完成")
+        convert_to_gpkg(i)
+        print(f"{i.stem} gpkg 转换完成")
+
+    # 打包 gpkg 文件为 zip
+    gpkg_file_path = dist_path.joinpath("天地图-行政区划.gpkg")
+    with zipfile.ZipFile(
+        dist_path.joinpath("行政区划.gpkg.zip"), "w", zipfile.ZIP_DEFLATED
+    ) as zipf:
+        zipf.write(gpkg_file_path, gpkg_file_path.relative_to(dist_path))
+
+    # 打包 shp 文件 为 zip
+
+    with zipfile.ZipFile(
+        dist_path.joinpath("行政区划.shp.zip"), "w", zipfile.ZIP_DEFLATED
+    ) as zipf:
+        directories = [p for p in dist_path.glob("*") if p.is_dir()]
+        for directory in directories:
+            for file in directory.glob("**/*"):
+                zipf.write(file, file.relative_to(directory.parent))
+    # 打包 geojson 文件 为 zip
+    with zipfile.ZipFile(
+        dist_path.joinpath("行政区划.geojson.zip"), "w", zipfile.ZIP_DEFLATED
+    ) as zipf:
+        for file in geojson_path.glob("**/*"):
+            zipf.write(file, file.relative_to(geojson_path))
+
+
+if __name__ == "__main__":
+    main()
